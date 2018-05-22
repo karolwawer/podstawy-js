90048d5f612e58662f3af4be561fa7c7b6c90d2a
Karol Wawer <karol.wawer.pl@gmail.com>
2018-05-22 13:09:33 +0200

    tablice

---
+++ js/tablice.js
@@ -0,0 +1,5 @@
+'use strict'
+//tablice indeksujemu od 0
+let tablica = ['Agata', 'Michal', 'Andrzej'];
+
+console.log(tablica[1]);
---
+++ js/typy.js
@@ -0,0 +1,18 @@
+'use strict'
+
+//zmienna niezdefiniowana
+//let x;
+//console.log(x);
+
+//zmienna null
+//let x = null;
+//console.log(x);
+
+//string
+//console.log('ala ma\n kota');
+
+//tablice[]
+
+
+
+
