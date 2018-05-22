18a19c271c12d305e14f498533d1b5815f7dfeff
Karol Wawer <karol.wawer.pl@gmail.com>
2018-05-22 09:55:35 +0200

    zmienne

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('123');
+//komentarze w JavaScript za pomocą ctrl + / lub shift ctrl /
+//console.log('123');
+'use strict';
+//zmienne
+
+let liczba = 10;
+let Liczba = 20;
+
+//duże liczby zarezerwowane są dla nazewnictwa klas
+console.log(liczba)
+console.log(Liczba)
